5dbecc02edff5f8bfd54323728db5cdea575f28c
Joseph Zhang <josephz885@gmail.com>
2022-07-06 23:03:47 -0500

    pointing to self yaml

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://josephz885.github.io/sw-api-docs/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
